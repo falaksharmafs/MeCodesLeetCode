a18b673c43166ceeb9f31b2585ed24bb5ebb5834
falaksharmafs <falaksharma23.06@gmail.com>
2026-07-26 01:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (57.80%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+         return "";
+
+        string first = strs[0];
+
+        for(int i = 0 ; i < first.length(); i++){
+           for(int j = 1 ; j < strs.size(); j++){
+              if(i >= strs[j].length() || first[i] != strs[j][i]){
+                return first.substr(0, i);
+              }
+           }
+
+        }
+        return first;
+        
+    }
+};
