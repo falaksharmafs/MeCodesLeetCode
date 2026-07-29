0378c64da9388265b2f61aeaf4f43d03aafefe72
falaksharmafs <falaksharma23.06@gmail.com>
2026-07-30 03:47:36 +0530

    Time: 129 ms (28.73%) | Memory: 83.3 MB (24.96%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+
+        unordered_set<string> st(begin(folder),end(folder));
+        vector<string>result;
+        for(string currFolder : folder){
+
+            bool isSubfolder = false;
+            string temp = currFolder;
+
+            while(!currFolder.empty()){
+                size_t position = currFolder.find_last_of('/');
+
+                currFolder = currFolder.substr(0,position);
+
+                if(st.find(currFolder) != st.end()){
+                    isSubfolder = true;
+                    break;
+                }
+            }
+
+            if(!isSubfolder){
+                result.push_back(temp);
+            }
+            
+        }
+        
+        return result;
+    }
+};
