166b7aca9895f1404b70b93f27bb01432bfae971
falaksharmafs <falaksharma23.06@gmail.com>
2026-07-30 03:08:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (15.25%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int n = s.length();
+        int count = 0;
+        int ch = s[0];
+        int changes = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]==ch){
+                count++;
+                continue;
+            }
+
+            if(count % 2== 0){
+                count =1;
+            }else{
+                changes +=1;
+                count = 0;
+            }
+            ch = s[i];
+        }
+        return changes;
+        
+    }
+};
