f0268fc66d22e3ede3d5971908e9cec1961a1711
falaksharmafs <falaksharma23.06@gmail.com>
2026-08-05 12:08:54 +0530

    Time: 1 ms (36.60%) | Memory: 13.1 MB (90.00%) - LeetSync

---
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+
+        long long count = 0;
+
+        for(char ch :s){
+            if(ch==c){
+                count++;
+            }
+        }
+
+        return ((count*(count+1)/2));
+        
+    }
+};
