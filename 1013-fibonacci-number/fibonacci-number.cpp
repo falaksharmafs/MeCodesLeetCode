87499edfba5327ed4d6016d83046513ddf197fec
falaksharmafs <falaksharma23.06@gmail.com>
2026-08-11 01:03:51 +0530

    Time: 10 ms (45.77%) | Memory: 7.8 MB (65.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-         return 0;
-        if(n==1)
-         return 1;
-        int ans = fib(n-1) + fib(n-2);
+        if (n <= 1) // base case is always the lowest possible value of param
+         return n;
 
-        return ans;  
+        return(fib(n-1)+fib(n-2)); 
+
+        
     }
 };
