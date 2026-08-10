6d5537b976c74a2a4062d98cf69742f0e2d3feb9
falaksharmafs <falaksharma23.06@gmail.com>
2026-08-11 01:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.75%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[46];
+    int solve(int n){
+        if(n<0)
+          return 0;
+
+          if(t[n]!=-1)
+            return t[n];
+        if(n==0)
+          return 1;
+        int onestep = solve(n-1);
+        int twostep = solve(n-2);
+
+        return t[n]=onestep+twostep;    
+    }
+    int climbStairs(int n) {
+
+        memset(t,-1,sizeof(t));
+        return solve(n);    
+    }
+};
